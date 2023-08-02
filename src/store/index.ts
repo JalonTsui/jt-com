c7ffab5cb277b1cd2386f4f0bde4241c4cbaffef
JalonTsui <123@qq.com>
2023-08-02 12:37:12 +0800

    finish setting pinia & router

---
+++ src/store/index.ts
@@ -0,0 +1,4 @@
+import { createPinia } from "pinia";
+const pinia = createPinia();
+// 扩展...
+export default pinia;
---
+++ src/store/test.ts
@@ -0,0 +1,11 @@
+import { defineStore } from "pinia";
+
+const useTest = defineStore("test-store", {
+  state() {
+    return {};
+  },
+  getters: {},
+  actions: {},
+});
+
+export { useTest };
